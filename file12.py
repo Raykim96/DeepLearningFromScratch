8febac14afb2ca88f0b35ab04189dd719ab0e31e
raykim <45217692+Raykim96@users.noreply.github.com>
2022-09-01 17:18:23 +0900

    Update file12.py

--- file12.py
+++ file12.py
@@ -1,8 +1,4 @@
-from sklearn.datasets import load_boston
-import pandas as pd
-boston = load_boston()
-dfx = pd.DataFrame(boston.data, columns = boston.feature_names)
-dfy = pd.DataFrame(boston.target, columns =["MEDV"])
 
 
-"""""""""exam"""""""""
+
+"hello from fred"
