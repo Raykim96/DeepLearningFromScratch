ea41402c76f7859176ba6a26dba5c88bd6c74a25
raykim <45217692+Raykim96@users.noreply.github.com>
2022-09-01 11:23:02 +0900

    Update file12.py

--- file12.py
+++ file12.py
@@ -1,16 +1,1 @@
-from sklearn.datasets import load_boston
-import pandas as pd
-boston = load_boston()
-dfx = pd.DataFrame(boston.data, columns = boston.feature_names)
-dfy = pd.DataFrame(boston.target, columns =["MEDV"])
-
-"20220901"
-
-"""""""""exam"""""""""
-##################
-" fixfixfix"
-###################
-
-"git에서 수정했어요 0901"
-
-"git에서 또또 수정했어요"
+"다 없앰 동료가"
